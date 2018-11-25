aced3bf1d4103d63ac9a3a643362a61d10132e17
Nick Inman <nick@theoatmeal.com>
2018-11-25 03:44:32 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,8 +3,11 @@
 def run_guessing_game
   guess = ""
   while guess != "exit"
-  puts "Guess a number between 1 and 6."
-  guess = gets.chomp
+    puts "Guess a number between 1 and 6."
+    guess.to_i = gets.chomp
+    random = rand(1..6)
+    if guess == random
+      
   end
-    puts "Goodbye"
+  puts "Goodbye"
 end
